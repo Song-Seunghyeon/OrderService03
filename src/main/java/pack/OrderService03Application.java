4f12f247ac6a4c9ad7a15d363f0020d00c88f864
S.Song <song000316@skuniv.ac.kr>
2026-08-07 09:39:06 +0900

    1

---
+++ src/main/java/pack/OrderService03Application.java
@@ -0,0 +1,22 @@
+package pack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class OrderService03Application {
+	
+	@GetMapping("/")
+	public String home() {
+		System.out.println("하이");
+		return"08:07";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderService03Application.class, args);
+	}
+
+}
